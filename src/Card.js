10000a238db0993da567452c25e70cc33f5a60a0
Kurt Pessa <kpessa@gmail.com>
2021-06-03 10:31:02 -0400

    working on deploying react app to github pages

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,21 @@
-import './css/Card.scss'
+import { useState } from 'react';
+
+import './css/Card.scss';
 
 const Card = ({ imgSrc, h1, p, color }) => {
+  const [hover, setHover] = useState(false);
+
+  const mouseOverHandler = () => setHover(true);
+  const mouseOutHandler = () => setHover(false);
+
   return (
     <section className="ccard" style={{ background: color }}>
       <img src={imgSrc} alt="icon" />
       <h1>{h1}</h1>
       <p>{p}</p>
-      <button style={{ color: color }}>Learn More</button>
+      <button style={{ color: !hover && color, background: hover && color }} onMouseOver={mouseOverHandler} onMouseOut={mouseOutHandler}>
+        Learn More
+      </button>
     </section>
   );
 };
